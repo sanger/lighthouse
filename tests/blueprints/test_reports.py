13948f703c889be0e40ed245f063aea7b3aa2b23
Eduardo Martin Rojo <emr@sanger.ac.uk>
2020-06-02 17:16:55 +0100

    Added some dummy tests to check functionality

---
+++ tests/blueprints/test_reports.py
@@ -0,0 +1,43 @@
+from http import HTTPStatus
+from unittest.mock import patch
+
+import responses
+import json
+
+
+def test_get_reports_endpoint(client):
+    with patch(
+        "lighthouse.blueprints.reports.get_reports_details", return_value=[],
+    ):
+        response = client.get("/reports")
+        assert response.status_code == HTTPStatus.OK
+
+
+def test_get_reports_list(client):
+    with patch(
+        "lighthouse.blueprints.reports.get_reports_details", return_value=[],
+    ):
+        response = client.get("/reports")
+        assert response.json == {"reports": []}
+
+
+def test_create_report(client, mocked_responses, app, tmp_path, samples):
+    with app.app_context():
+        # Mock of labwhere
+        labwhere_url = f"http://{app.config['LABWHERE_URL']}/api/labwares/searches"
+        body = json.dumps([{"barcode": "123", "location": {"barcode": "4567"}}])
+        mocked_responses.add(
+            responses.POST, labwhere_url, body=body, status=HTTPStatus.OK,
+        )
+
+        with patch(
+            "lighthouse.jobs.reports.get_new_report_name_and_path",
+            return_value=["test.xlsx", f"{tmp_path}/test.xlsx"],
+        ):
+            with patch(
+                "lighthouse.blueprints.reports.get_reports_details",
+                return_value="Some details of a report",
+            ):
+                # mocked_response.add("http://{app.config['LABWHERE_URL']}/api/labwares/searches"
+                response = client.post("/reports/new")
+                assert response.json == {"reports": "Some details of a report"}
