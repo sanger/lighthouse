b1ee51c7239b628554cf96cf60efaa28066dda50
Katy Taylor <kt17@sanger.ac.uk>
2020-09-15 16:57:58 +0100

    nicer query names

--- setup_test_db.py
+++ setup_test_db.py
@@ -9,13 +9,13 @@ print("Initialising the test MySQL warehouse database")
 create_engine_string = f"mysql+pymysql://root:root@localhost/unified_warehouse_test"
 sql_engine = sqlalchemy.create_engine(create_engine_string, pool_recycle=3600)
 
-sql_string_1 = """
+create_db = """
 CREATE DATABASE IF NOT EXISTS `unified_warehouse_test` /*!40100 DEFAULT CHARACTER SET latin1 */;
 """
-sql_string_2 = """
+drop_table = """
 DROP TABLE IF EXISTS `unified_warehouse_test`.`lighthouse_sample`;
 """
-sql_string_3 = """
+create_table = """
 CREATE TABLE `unified_warehouse_test`.`lighthouse_sample` (
 `id` int NOT NULL AUTO_INCREMENT,
 `mongodb_id` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'Auto-generated id from MongoDB',
@@ -41,8 +41,8 @@ KEY `index_lighthouse_sample_on_date_tested` (`date_tested`)
 """
 
 with sql_engine.connect() as connection:
-  connection.execute(sql_string_1)
-  connection.execute(sql_string_2)
-  connection.execute(sql_string_3)
+  connection.execute(create_db)
+  connection.execute(drop_table)
+  connection.execute(create_table)
 
 print("Done")
