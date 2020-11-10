676e5a876407bb1cc8ba66f4f96631c12fb49efb
Eduardo Martin Rojo <emr@sanger.ac.uk>
2020-11-10 12:30:26 +0000

    Connect to localhost for sql server test

--- setup_sqlserver_test_db.py
+++ setup_sqlserver_test_db.py
@@ -2,7 +2,7 @@ import lighthouse.config.test as config  # type: ignore
 import pyodbc  # type: ignore
 
 cnxn = pyodbc.connect(
-    "DRIVER={ODBC Driver 17 for SQL Server};SERVER=tcp:host.docker.internal;DATABASE=master;UID=SA;PWD=MyS3cr3tPassw0rd",
+    "DRIVER={ODBC Driver 17 for SQL Server};SERVER=tcp:localhost;DATABASE=master;UID=SA;PWD=MyS3cr3tPassw0rd",
     autocommit=True,
 )
 cursor = cnxn.cursor()
