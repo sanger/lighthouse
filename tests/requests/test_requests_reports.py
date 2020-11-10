a3f49aa4c688204ad3cec93fd0e12a5f77a0b0b1
Katy Taylor <kt17@sanger.ac.uk>
2020-11-10 13:51:11 +0000

    fix other tests by changing method mocking to return correct format

--- tests/requests/test_requests_reports.py
+++ tests/requests/test_requests_reports.py
@@ -2,13 +2,16 @@ from http import HTTPStatus
 from unittest.mock import patch
 import pandas as pd
 
-from lighthouse.constants import FIELD_ROOT_SAMPLE_ID
+from lighthouse.constants import FIELD_ROOT_SAMPLE_ID, FIELD_PLATE_BARCODE, FIELD_RESULT, FIELD_COORDINATE
 
 
 def test_create_report(client, samples, samples_declarations, labwhere_samples_simple):
     with patch(
         "lighthouse.helpers.reports.get_cherrypicked_samples",
-        return_value=pd.DataFrame(["MCM001"], columns=[FIELD_ROOT_SAMPLE_ID]),
+        return_value=pd.DataFrame(
+            [["MCM001", "pb_1", "Positive", "A1"]],
+            columns=[FIELD_ROOT_SAMPLE_ID, FIELD_PLATE_BARCODE, FIELD_RESULT, FIELD_COORDINATE]
+        )
     ):
         response = client.post(
             "/reports/new",
