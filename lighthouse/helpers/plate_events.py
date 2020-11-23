d2b2016007e9eaae251272389ca1540170ff0b37
Christopher Friend <christopher.friend@tessella.com>
2020-11-23 15:24:35 +0000

    Partially implement constructing unrecognised plate messages

--- lighthouse/helpers/plate_events.py
+++ lighthouse/helpers/plate_events.py
@@ -1,4 +1,8 @@
+import logging
 from typing import Optional, Dict, Tuple, List
+from uuid import uuid4
+from datetime import datetime
+from flask import current_app as app
 from lighthouse.messages.message import Message  # type: ignore
 from lighthouse.constants import (
     PLATE_EVENT_SOURCE_COMPLETED,
@@ -6,6 +10,8 @@ from lighthouse.constants import (
     PLATE_EVENT_SOURCE_NO_MAP_DATA,
     PLATE_EVENT_SOURCE_ALL_NEGATIVES,
 )
+
+logger = logging.getLogger(__name__)
 
 
 def construct_event_message(
@@ -76,7 +82,46 @@ def construct_source_plate_not_recognised_message(
         {[str]} -- Any errors attempting to construct the message, otherwise an empty array.
         {Message} -- The constructed message; otherwise None if there are any errors.
     """
-    return ["Not implemented"], None
+    try:
+        user_id = params.get("user_id", "")
+        robot_serial_number = params.get("robot", "")
+        if len(user_id) == 0 or len(robot_serial_number) == 0:
+            return [
+                "'user_id' and 'robot' are required to construct a "
+                f"{PLATE_EVENT_SOURCE_NOT_RECOGNISED} event message"
+            ], None
+
+        robot_uuid = get_robot_uuid(robot_serial_number)
+        if robot_uuid is None:
+            return [f"Unable to determine a uuid for robot '{robot_serial_number}'"], None
+
+        robot_uuid = str(uuid4())  # TODO - get robot uuid from config
+        message_content = {
+            "event": {
+                "uuid": str(uuid4()),
+                "event_type": PLATE_EVENT_SOURCE_NOT_RECOGNISED,
+                "occured_at": get_current_datetime(),
+                "user_identifier": user_id,
+                "subjects": [
+                    {
+                        "role_type": "robot",
+                        "subject_type": "robot",
+                        "friendly_name": robot_serial_number,
+                        "uuid": robot_uuid,
+                    }
+                ],
+                "metadata": {},
+            },
+            "lims": app.config["RMQ_LIMS_ID"],
+        }
+        return [], Message(message_content)
+    except Exception as e:
+        logger.error(f"Failed to construct a {PLATE_EVENT_SOURCE_NOT_RECOGNISED} message")
+        logger.exception(e)
+        return [
+            "An unexpected error occurred attempting to construct the "
+            f"{PLATE_EVENT_SOURCE_NOT_RECOGNISED} event message"
+        ], None
 
 
 def construct_source_plate_no_map_data_message(
@@ -109,3 +154,26 @@ def construct_source_plate_all_negatives_message(
         {Message} -- The constructed message; otherwise None if there are any errors.
     """
     return ["Not implemented"], None
+
+
+def get_current_datetime() -> str:
+    """Returns the current datetime in a format compatible with messaging.
+
+    Returns:
+        {str} -- The current datetime.
+    """
+    return datetime.now().isoformat(timespec="seconds")
+
+
+def get_robot_uuid(serial_number: str) -> str:
+    """Maps a robot serial number to a uuid.
+
+    Arguments:
+        params {str} -- The robot serial number.
+
+    Returns:
+        {str} -- The robot uuid.
+    """
+    return str(uuid4())  # TODO - get robot uuid from config
+
+
