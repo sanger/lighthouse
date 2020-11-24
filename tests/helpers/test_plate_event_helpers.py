25d2b363b0d9cd695582c7a77d535f20c270a11d
Christopher Friend <christopher.friend@tessella.com>
2020-11-24 10:49:00 +0000

    Add tests for constructing source plate all negatives message

--- tests/helpers/test_plate_event_helpers.py
+++ tests/helpers/test_plate_event_helpers.py
@@ -5,6 +5,7 @@ from lighthouse.helpers.plate_events import (
     get_routing_key,
     construct_source_plate_not_recognised_message,
     construct_source_plate_no_map_data_message,
+    construct_source_plate_all_negatives_message,
 )
 from lighthouse.constants import (
     PLATE_EVENT_SOURCE_COMPLETED,
@@ -205,7 +206,7 @@ def test_construct_source_plate_no_map_data_message_errors_without_user_id():
     assert message is None
 
 
-def test_construct_source_plate_no_map_data_message_errors_without_user_id():
+def test_construct_source_plate_no_map_data_message_errors_without_robot():
     test_params = {"barcode": "ABC123", "user_id": "test user"}
     errors, message = construct_source_plate_no_map_data_message(test_params)
 
@@ -289,6 +290,140 @@ def test_construct_source_plate_no_map_data_message_creates_expected_message(app
                     event = message_content["event"]
                     assert event["uuid"] is not None
                     assert event["event_type"] == PLATE_EVENT_SOURCE_NO_MAP_DATA
+                    assert event["occured_at"] is not None
+                    assert event["user_identifier"] == test_user_id
+
+                    subjects = event["subjects"]
+                    assert len(subjects) == 2
+                    assert {  # robot subject
+                        "role_type": "robot",
+                        "subject_type": "robot",
+                        "friendly_name": test_robot_serial_number,
+                        "uuid": test_robot_uuid,
+                    } in subjects
+                    assert {
+                        "role_type": "cherrypicking_source_labware",
+                        "subject_type": "plate",
+                        "friendly_name": test_barcode,
+                        "uuid": test_source_plate_uuid,
+                    } in subjects
+
+
+# ---------- construct_source_plate_all_negatives_message tests ----------
+
+
+def test_construct_source_plate_all_negatives_message_errors_without_barcode():
+    test_params = {"user_id": "test user", "robot": "12345"}
+    errors, message = construct_source_plate_all_negatives_message(test_params)
+
+    assert len(errors) == 1
+    assert message is None
+
+    test_params = {"barcode": "", "user_id": "test user", "robot": "12345"}
+    errors, message = construct_source_plate_all_negatives_message(test_params)
+
+    assert len(errors) == 1
+    assert message is None
+
+
+def test_construct_source_plate_all_negatives_message_errors_without_user_id():
+    test_params = {"barcode": "ABC123", "robot": "12345"}
+    errors, message = construct_source_plate_all_negatives_message(test_params)
+
+    assert len(errors) == 1
+    assert message is None
+
+    test_params = {"barcode": "ABC123", "user_id": "", "robot": "12345"}
+    errors, message = construct_source_plate_all_negatives_message(test_params)
+
+    assert len(errors) == 1
+    assert message is None
+
+
+def test_construct_source_plate_all_negatives_message_errors_without_robot():
+    test_params = {"barcode": "ABC123", "user_id": "test user"}
+    errors, message = construct_source_plate_all_negatives_message(test_params)
+
+    assert len(errors) == 1
+    assert message is None
+
+    test_params = {"barcode": "ABC123", "user_id": "test user", "robot": ""}
+    errors, message = construct_source_plate_all_negatives_message(test_params)
+
+    assert len(errors) == 1
+    assert message is None
+
+
+def test_construct_source_plate_all_negatives_message_errors_with_failure_getting_robot_uuid():
+    with patch("lighthouse.helpers.plate_events.__get_robot_uuid", side_effect=Exception("Boom!")):
+        test_params = {"barcode": "ABC123", "user_id": "test_user", "robot": "12345"}
+        errors, message = construct_source_plate_all_negatives_message(test_params)
+
+        assert len(errors) == 1
+        assert message is None
+
+
+def test_construct_source_plate_all_negatives_message_errors_without_robot_uuid():
+    with patch("lighthouse.helpers.plate_events.__get_robot_uuid", return_value=None):
+        test_params = {"barcode": "ABC123", "user_id": "test_user", "robot": "12345"}
+        errors, message = construct_source_plate_all_negatives_message(test_params)
+
+        assert len(errors) == 1
+        assert message is None
+
+
+def test_construct_source_plate_all_negatives_message_errors_with_failure_getting_plate_uuid():
+    with patch(
+        "lighthouse.helpers.plate_events.__get_source_plate_uuid", side_effect=Exception("Boom!")
+    ):
+        test_params = {"barcode": "ABC123", "user_id": "test_user", "robot": "12345"}
+        errors, message = construct_source_plate_all_negatives_message(test_params)
+
+        assert len(errors) == 1
+        assert message is None
+
+
+def test_construct_source_plate_all_negatives_message_errors_without_source_plate_uuid():
+    with patch("lighthouse.helpers.plate_events.__get_source_plate_uuid", return_value=None):
+        test_params = {"barcode": "ABC123", "user_id": "test_user", "robot": "12345"}
+        errors, message = construct_source_plate_all_negatives_message(test_params)
+
+        assert len(errors) == 1
+        assert message is None
+
+
+def test_construct_source_plate_all_negatives_message_creates_expected_message(app):
+    with app.app_context():
+        test_robot_uuid = "adb83afb-d59a-47f2-8d2d-7f71703ca256"
+        with patch(
+            "lighthouse.helpers.plate_events.__get_robot_uuid", return_value=test_robot_uuid
+        ):
+            test_source_plate_uuid = "3a06a935-0029-49ea-81bc-e5d8eeb1319e"
+            with patch(
+                "lighthouse.helpers.plate_events.__get_source_plate_uuid",
+                return_value=test_source_plate_uuid,
+            ):
+                with patch("lighthouse.helpers.plate_events.Message") as mock_message:
+                    test_barcode = "ABC123"
+                    test_user_id = "test_user"
+                    test_robot_serial_number = "12345"
+                    test_params = {
+                        "barcode": test_barcode,
+                        "user_id": test_user_id,
+                        "robot": test_robot_serial_number,
+                    }
+                    errors, _ = construct_source_plate_all_negatives_message(test_params)
+
+                    assert len(errors) == 0
+
+                    args, _ = mock_message.call_args
+                    message_content = args[0]
+
+                    assert message_content["lims"] == app.config["RMQ_LIMS_ID"]
+
+                    event = message_content["event"]
+                    assert event["uuid"] is not None
+                    assert event["event_type"] == PLATE_EVENT_SOURCE_ALL_NEGATIVES
                     assert event["occured_at"] is not None
                     assert event["user_identifier"] == test_user_id
 
