22a56dedfc1cc68257e184f7fea71ec0b19402c0
Christopher Friend <christopher.friend@tessella.com>
2020-11-30 11:56:40 +0000

    Update robot subject tests to reflect that an external service is called

--- tests/helpers/test_plates_helpers.py
+++ tests/helpers/test_plates_helpers.py
@@ -804,18 +804,10 @@ def test_create_cherrypicked_post_body(app):
         )
 
 
-def test_robot_subject_no_robot_mapping_in_config(app):
-    with app.app_context():
-        del app.config["BECKMAN_ROBOTS"]
+def test_robot_subject_no_robot_uuid():
+    with patch("lighthouse.helpers.plates.get_robot_uuid", return_value=None):
         with pytest.raises(Exception):
             robot_subject("1234")
-
-
-def test_robot_subject_no_robot_uuid(app):
-    with app.app_context():
-        del app.config["BECKMAN_ROBOTS"]["BKRB0001"]["uuid"]
-        with pytest.raises(Exception):
-            robot_subject("BKRB0001")
 
 
 def test_find_samples_returns_none_if_no_query_provided(app):
