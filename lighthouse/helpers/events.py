62484c630e76ac5293c66ecb6b7613a167c5bc48
Christopher Friend <christopher.friend@tessella.com>
2020-11-30 13:35:17 +0000

    Add method to construct destination plate message subject

--- lighthouse/helpers/events.py
+++ lighthouse/helpers/events.py
@@ -1,4 +1,6 @@
 from flask import current_app as app
+from typing import Dict
+from uuid import uuid4
 
 
 def get_routing_key(event_type: str) -> str:
@@ -11,3 +13,20 @@ def get_routing_key(event_type: str) -> str:
         {str} -- The message routing key.
     """
     return app.config["RMQ_ROUTING_KEY"].replace("#", event_type)
+
+
+def construct_destination_plate_message_subject(barcode: str) -> Dict[str, str]:
+    """Constructs a message subject for a cherrypicking destination plate.
+
+    Arguments:
+        barcode {str} -- The destination plate's barcode.
+
+    Returns:
+        {Dict[str, str]} -- The destination plate message subject.
+    """
+    return {
+        "role_type": "cherrypicking_destination_labware",
+        "subject_type": "plate",
+        "friendly_name": barcode,
+        "uuid": str(uuid4()),
+    }
--- tests/helpers/test_events.py
+++ tests/helpers/test_events.py
@@ -1,4 +1,9 @@
-from lighthouse.helpers.events import get_routing_key
+from uuid import uuid4
+from unittest.mock import patch
+from lighthouse.helpers.events import (
+    get_routing_key,
+    construct_destination_plate_message_subject,
+)
 
 
 # ---------- get_routing_key tests ----------
@@ -10,3 +15,23 @@ def test_get_routing_key(app):
         result = get_routing_key(test_event_type)
 
         assert result == f"test.event.{test_event_type}"
+
+
+# ---------- construct_destination_plate_message_subject tests ----------
+
+
+def test_construct_destination_plate_message_subject():
+    test_uuid = uuid4()
+    with patch("lighthouse.helpers.events.uuid4", return_value=test_uuid):
+        barcode = "ABC123"
+        expected_subject = {
+            "role_type": "cherrypicking_destination_labware",
+            "subject_type": "plate",
+            "friendly_name": barcode,
+            "uuid": str(test_uuid),
+        }
+
+        result = construct_destination_plate_message_subject(barcode)
+        assert result == expected_subject
+        
+
