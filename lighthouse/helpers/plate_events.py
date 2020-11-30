b780d30c2e9bc561cc1f905a43280f87ccaba662
Christopher Friend <christopher.friend@tessella.com>
2020-11-30 13:41:19 +0000

    Include missed code change

--- lighthouse/helpers/plate_events.py
+++ lighthouse/helpers/plate_events.py
@@ -49,18 +49,6 @@ def construct_event_message(
         return [f"Unrecognised event type '{event_type}'"], None
 
 
-def get_routing_key(event_type: str) -> str:
-    """Determines the routing key for a plate event message.
-
-    Arguments:
-        event_type {str} -- The event type for which to determine a routing key
-
-    Returns:
-        {str} -- The message routing key.
-    """
-    return app.config["RMQ_ROUTING_KEY"].replace("#", event_type)
-
-
 def construct_source_plate_completed_message(
     params: Dict[str, str]
 ) -> Tuple[List[str], Optional[Message]]:
