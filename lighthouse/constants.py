f3d66633a66c6e1fd1411699fcf448d3d25a2088
James Glover <james.glover@sanger.ac.uk>
2020-12-01 14:06:50 +0000

    Document event constants

--- lighthouse/constants.py
+++ lighthouse/constants.py
@@ -89,7 +89,12 @@ POSITIVE_SAMPLES_MONGODB_FILTER = {
     ],
 }
 
+# Events detailed in https://ssg-confluence.internal.sanger.ac.uk/display/PSDPUB/Cherrypicking+Events
+# Source plate has had all pickable wells cherrypicked into destination plates, and the plate is put into the output stacks.
 PLATE_EVENT_SOURCE_COMPLETED = "lh_beckman_cp_source_completed"
+# Source plate barcode cannot be read (damaged or missing), and the plate is put into the output stacks.
 PLATE_EVENT_SOURCE_NOT_RECOGNISED = "lh_beckman_cp_source_plate_unrecognised"
+# Source plate has no related plate map data, cannot be cherrypicked (yet), and the plate is returned to the input stacks.
 PLATE_EVENT_SOURCE_NO_MAP_DATA = "lh_beckman_cp_source_no_plate_map_data"
+# Source plate only contains negatives, nothing to cherrypick, and the plate is put into the output stacks.
 PLATE_EVENT_SOURCE_ALL_NEGATIVES = "lh_beckman_cp_source_all_negatives"
