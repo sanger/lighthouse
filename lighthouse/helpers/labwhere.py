ceb51b7b16644a7bb1e70c7c95f753e3f4f0e71b
James Glover <james.glover@sanger.ac.uk>
2020-12-01 14:10:03 +0000

    Add labwhere set location method

--- lighthouse/helpers/labwhere.py
+++ lighthouse/helpers/labwhere.py
@@ -21,3 +21,37 @@ def get_locations_from_labwhere(labware_barcodes):
         json={"barcodes": labware_barcodes},
     )
 
+
+def set_locations_in_labwhere(
+    labware_barcodes: List[str], location_barcode: str, user_barcode: str
+):
+    """Record a scan event in labwhere for labware_barcodes
+
+    Note: The labwhere API currently identifies users by their swipecard, however
+    currently we only have access to the login when generating plate events.
+    We can use the robot as a user instead, this has the advantage that:
+      1) Its not open to user error
+      2) There is reduced risk that the robot is not registered in labwhere
+      3) We wont ascribe unloading the robot to the same user who loaded it
+      4) It will not require changes to labwhere or additional user id validation
+
+    Arguments:
+        labware_barcodes {List[str]} - The source plate barcodes
+        location_barcode {str} - The barcode of the locations to which the labwares have been transferred.
+        user_barcode {str} - The swipecard/barcode for the user or robot associate with the scan.
+
+
+    Returns:
+        {requests.Response} -- The labwhere response object
+    """
+
+    return requests.post(
+        f"http://{app.config['LABWHERE_URL']}/api/scans",
+        json={
+            "scan": {
+                "user_code": user_barcode,
+                "labware_barcodes": "\n".join(labware_barcodes),
+                "location_barcode": location_barcode,
+            }
+        },
+    )
