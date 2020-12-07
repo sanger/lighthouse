bed41c9ec76a45d96fa27b85c78a5829c902dd47
Eduardo Martin Rojo <emr@sanger.ac.uk>
2020-12-07 13:41:00 +0000

    Readded coment

--- lighthouse/helpers/labwhere.py
+++ lighthouse/helpers/labwhere.py
@@ -12,7 +12,7 @@ from typing import List
 
 
 def get_locations_from_labwhere(labware_barcodes):
-    """
+    """Retrieve a location from labwhere
     Example record from labwhere:
     { 'barcode': 'GLA001024R', 'location_barcode': 'lw-uk-biocentre-box-gsw--98-14813'}
     """
