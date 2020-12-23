1f36f500025df5abf4cb7fec688be424002ca31f
Christopher Friend <christopher.friend@tessella.com>
2020-12-23 11:45:04 +0000

    Add beckman events to the events warehouse test data

--- tests/data/fixture_data.py
+++ tests/data/fixture_data.py
@@ -557,10 +557,14 @@ EVENT_WH_DATA: Dict[str, Any] = {
     "subjects": [
         {"id": 1, "uuid": "1".encode("utf-8"), "friendly_name": "ss1", "subject_type_id": 1},
         {"id": 2, "uuid": "2".encode("utf-8"), "friendly_name": "ss2", "subject_type_id": 1},
+        {"id": 3, "uuid": "3".encode("utf-8"), "friendly_name": "beck-ss1", "subject_type_id": 1},
+        {"id": 4, "uuid": "4".encode("utf-8"), "friendly_name": "beck-ss2", "subject_type_id": 1},
     ],
     "roles": [
         {"id": 1, "event_id": 1, "subject_id": 1, "role_type_id": 1},
         {"id": 2, "event_id": 2, "subject_id": 2, "role_type_id": 1},
+        {"id": 3, "event_id": 3, "subject_id": 3, "role_type_id": 1},
+        {"id": 4, "event_id": 4, "subject_id": 4, "role_type_id": 1},
     ],
     "events": [
         {
@@ -579,8 +583,27 @@ EVENT_WH_DATA: Dict[str, Any] = {
             "occured_at": "2015-11-25 11:35:30",
             "user_identifier": "test@example.com",
         },
+        {
+            "id": 3,
+            "lims_id": "SQSCP",
+            "uuid": "3".encode("utf-8"),
+            "event_type_id": 2,
+            "occured_at": "2015-11-25 11:35:30",
+            "user_identifier": "test@example.com",
+        },
+        {
+            "id": 4,
+            "lims_id": "SQSCP",
+            "uuid": "4".encode("utf-8"),
+            "event_type_id": 2,
+            "occured_at": "2015-11-25 11:35:30",
+            "user_identifier": "test@example.com",
+        },
     ],
-    "event_types": [{"id": 1, "key": "cherrypick_layout_set", "description": "stuff"}],
+    "event_types": [
+        {"id": 1, "key": "cherrypick_layout_set", "description": "stuff"},
+        {"id": 2, "key": "lh_beckman_cp_destination_created", "description": "stuff"},
+    ],
     "subject_types": [{"id": 1, "key": "sample", "description": "stuff"}],
     "role_types": [{"id": 1, "key": "sample", "description": "stuff"}],
 }
