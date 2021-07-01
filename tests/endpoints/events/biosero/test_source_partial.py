098542f274e8a463ebc23e7bd2f3645b17bdc671
Eduardo Martin Rojo <emr@sanger.ac.uk>
2021-07-01 22:07:15 +0100

    Rename property

--- tests/endpoints/events/biosero/test_source_partial.py
+++ tests/endpoints/events/biosero/test_source_partial.py
@@ -200,6 +200,6 @@ def test_post_event_partially_completed_with_validation_error_after_storing_in_m
                     "plate_barcode": ["'barcode' should not contain any whitespaces"],
                     "robot_serial_number": ["'robot' should not contain any whitespaces"],
                     "source_plate_uuid": ["'barcode' should not contain any whitespaces"],
-                    "picked_samples_from_source": ["'barcode' should not contain any whitespaces"],
+                    "samples_picked_from_source": ["'barcode' should not contain any whitespaces"],
                     "robot_uuid": ["'robot' should not contain any whitespaces"],
                 }
