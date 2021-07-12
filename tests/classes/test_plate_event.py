f5eeb290357c71d96026fe926e5714fb490170f3
Stuart McHattie <sm49@sanger.ac.uk>
2021-07-12 11:23:53 +0100

    Rename dummy class so that pytest doesn't try to collect it
    Classes that start Test will be auto-collected and we don't need this to be

--- tests/classes/test_plate_event.py
+++ tests/classes/test_plate_event.py
@@ -6,26 +6,26 @@ from pytest import raises
 from lighthouse.classes.events import EventNotInitializedError, PlateEvent
 
 
-class TestDummy(PlateEvent):
+class DummyEvent(PlateEvent):
     def _create_message(self):
         return None
 
 
 def test_source_partial_new(app):
-    event = TestDummy("source_partial", plate_type=PlateEvent.PlateTypeEnum.SOURCE)
+    event = DummyEvent("source_partial", plate_type=PlateEvent.PlateTypeEnum.SOURCE)
     assert event.event_type == "source_partial"
     assert event.state == PlateEvent.EVENT_NOT_INITIALIZED
 
 
 def test_process_event_uninitialized(app):
-    event = TestDummy(event_type="source_partial", plate_type=PlateEvent.PlateTypeEnum.SOURCE)
+    event = DummyEvent(event_type="source_partial", plate_type=PlateEvent.PlateTypeEnum.SOURCE)
     with raises(EventNotInitializedError):
         event.process_event()
 
 
 def test_initialize_event(app):
     mytime = datetime.now()
-    event = TestDummy(event_type="source_partial", plate_type=PlateEvent.PlateTypeEnum.SOURCE)
+    event = DummyEvent(event_type="source_partial", plate_type=PlateEvent.PlateTypeEnum.SOURCE)
 
     with raises(EventNotInitializedError):
         event.initialize_event({"_created": mytime})
@@ -43,7 +43,7 @@ def test_initialize_event(app):
 
 def test_process_event(app):
     with app.app_context():
-        event = TestDummy(event_type="source_partial", plate_type=PlateEvent.PlateTypeEnum.SOURCE)
+        event = DummyEvent(event_type="source_partial", plate_type=PlateEvent.PlateTypeEnum.SOURCE)
         with patch.object(event, "_create_message") as create_message:
             with patch.object(event, "send_warehouse_message") as send_warehouse_message:
                 event.initialize_event({"event_wh_uuid": "uuid", "_created": datetime.now()})
@@ -55,7 +55,7 @@ def test_process_event(app):
 
 def test_build_new_warehouse_message(app):
     with app.app_context():
-        event = TestDummy(event_type="source_partial", plate_type=PlateEvent.PlateTypeEnum.SOURCE)
+        event = DummyEvent(event_type="source_partial", plate_type=PlateEvent.PlateTypeEnum.SOURCE)
         with raises(EventNotInitializedError):
             event.build_new_warehouse_message()
 
@@ -65,7 +65,7 @@ def test_build_new_warehouse_message(app):
 
 def test_errors(app):
     with app.app_context():
-        event = TestDummy(event_type="source_partial", plate_type=PlateEvent.PlateTypeEnum.SOURCE)
+        event = DummyEvent(event_type="source_partial", plate_type=PlateEvent.PlateTypeEnum.SOURCE)
         assert event.errors == {}
         mock = MagicMock()
         mock.errors = ["an error"]
@@ -78,7 +78,7 @@ def test_errors(app):
 
 def test_is_valid(app):
     with app.app_context():
-        event = TestDummy(event_type="source_partial", plate_type=PlateEvent.PlateTypeEnum.SOURCE)
+        event = DummyEvent(event_type="source_partial", plate_type=PlateEvent.PlateTypeEnum.SOURCE)
         assert event.is_valid() is True
 
         mock = MagicMock()
@@ -94,7 +94,7 @@ def test_is_valid(app):
 
 def test_process_errors(app):
     with app.app_context():
-        event = TestDummy(event_type="source_partial", plate_type=PlateEvent.PlateTypeEnum.SOURCE)
+        event = DummyEvent(event_type="source_partial", plate_type=PlateEvent.PlateTypeEnum.SOURCE)
         event.initialize_event({"event_wh_uuid": "uuid", "_created": datetime.now()})
 
         with patch("lighthouse.classes.events.plate_event.set_errors_to_event") as mock:
@@ -110,7 +110,7 @@ def test_process_errors(app):
 
 def test_process_exception(app):
     with app.app_context():
-        event = TestDummy(event_type="source_partial", plate_type=PlateEvent.PlateTypeEnum.SOURCE)
+        event = DummyEvent(event_type="source_partial", plate_type=PlateEvent.PlateTypeEnum.SOURCE)
         event.initialize_event({"event_wh_uuid": "uuid", "_created": datetime.now()})
 
         exc = Exception("boom!")
