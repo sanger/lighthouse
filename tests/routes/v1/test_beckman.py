f7b46c4f0b0d662d04950da1a0fcdc16e318c1a5
Stuart McHattie <sm49@sanger.ac.uk>
2021-07-22 16:53:20 +0100

    Merge prefixes on to endpoints before parametrizing tests

--- tests/routes/v1/test_beckman.py
+++ tests/routes/v1/test_beckman.py
@@ -6,14 +6,17 @@ ROBOTS_ENDPOINT = "/beckman/robots"
 FAILURE_TYPES_ENDPOINT = "/beckman/failure-types"
 ENDPOINT_PREFIXES = ["", "/v1"]
 
+ROBOTS_BASE_URLS = [prefix + ROBOTS_ENDPOINT for prefix in ENDPOINT_PREFIXES]
+FAILURE_TYPES_BASE_URLS = [prefix + FAILURE_TYPES_ENDPOINT for prefix in ENDPOINT_PREFIXES]
+
 
 # ---------- get_robots tests ----------
 
 
-@pytest.mark.parametrize("prefix", ENDPOINT_PREFIXES)
-def test_get_robots_returns_expected_robots(app, client, prefix):
+@pytest.mark.parametrize("base_url", ROBOTS_BASE_URLS)
+def test_get_robots_returns_expected_robots(app, client, base_url):
     with app.app_context():
-        response = client.get(prefix + ROBOTS_ENDPOINT)
+        response = client.get(base_url)
 
         assert response.status_code == HTTPStatus.OK
         assert len(response.json["errors"]) == 0
@@ -25,22 +28,22 @@ def test_get_robots_returns_expected_robots(app, client, prefix):
         ]
 
 
-@pytest.mark.parametrize("prefix", ENDPOINT_PREFIXES)
-def test_get_robots_internal_server_error_no_robots_config_exists(app, client, prefix):
+@pytest.mark.parametrize("base_url", ROBOTS_BASE_URLS)
+def test_get_robots_internal_server_error_no_robots_config_exists(app, client, base_url):
     with app.app_context():
         del app.config["BECKMAN_ROBOTS"]
-        response = client.get(prefix + ROBOTS_ENDPOINT)
+        response = client.get(base_url)
 
         assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
         assert len(response.json["errors"]) == 1
         assert len(response.json["robots"]) == 0
 
 
-@pytest.mark.parametrize("prefix", ENDPOINT_PREFIXES)
-def test_get_robots_internal_server_error_incorrect_format_config(app, client, prefix):
+@pytest.mark.parametrize("base_url", ROBOTS_BASE_URLS)
+def test_get_robots_internal_server_error_incorrect_format_config(app, client, base_url):
     with app.app_context():
         app.config["BECKMAN_ROBOTS"] = {"BKRB0001": {"no name key": "not a name"}}
-        response = client.get(prefix + ROBOTS_ENDPOINT)
+        response = client.get(base_url)
 
         assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
         assert len(response.json["errors"]) == 1
@@ -50,10 +53,10 @@ def test_get_robots_internal_server_error_incorrect_format_config(app, client, p
 # ---------- get_failure_types tests ----------
 
 
-@pytest.mark.parametrize("prefix", ENDPOINT_PREFIXES)
-def test_get_failure_types_returns_expected_failure_types(app, client, prefix):
+@pytest.mark.parametrize("base_url", FAILURE_TYPES_BASE_URLS)
+def test_get_failure_types_returns_expected_failure_types(app, client, base_url):
     with app.app_context():
-        response = client.get(prefix + FAILURE_TYPES_ENDPOINT)
+        response = client.get(base_url)
 
         assert response.status_code == HTTPStatus.OK
         assert len(response.json["errors"]) == 0
@@ -64,22 +67,22 @@ def test_get_failure_types_returns_expected_failure_types(app, client, prefix):
         ]
 
 
-@pytest.mark.parametrize("prefix", ENDPOINT_PREFIXES)
-def test_get_failure_types_internal_server_error_no_failure_types_config_exists(app, client, prefix):
+@pytest.mark.parametrize("base_url", FAILURE_TYPES_BASE_URLS)
+def test_get_failure_types_internal_server_error_no_failure_types_config_exists(app, client, base_url):
     with app.app_context():
         del app.config["BECKMAN_FAILURE_TYPES"]
-        response = client.get(prefix + FAILURE_TYPES_ENDPOINT)
+        response = client.get(base_url)
 
         assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
         assert len(response.json["errors"]) == 1
         assert len(response.json["failure_types"]) == 0
 
 
-@pytest.mark.parametrize("prefix", ENDPOINT_PREFIXES)
-def test_get_failure_types_internal_server_error_incorrect_format_config(app, client, prefix):
+@pytest.mark.parametrize("base_url", FAILURE_TYPES_BASE_URLS)
+def test_get_failure_types_internal_server_error_incorrect_format_config(app, client, base_url):
     with app.app_context():
         app.config["BECKMAN_FAILURE_TYPES"] = ["not the expected dictionary"]
-        response = client.get(prefix + FAILURE_TYPES_ENDPOINT)
+        response = client.get(base_url)
 
         assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
         assert len(response.json["errors"]) == 1
