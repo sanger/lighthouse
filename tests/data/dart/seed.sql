27770db7448ff88d8dce82984e9b7809a64c6303
tc23sanger <tc23@sanger.ac.uk>
2021-12-05 18:48:52 +0000

    Fix unit test data

--- tests/data/dart/seed.sql
+++ tests/data/dart/seed.sql
@@ -11,10 +11,10 @@ VALUES
 --    |              |      |             |      |           |root_sample_id NVARCHAR(50),
 --    |              |      |             |      |           |            |rna_id NVARCHAR(50),
 --    |              |      |             |      |           |            |        |lab_id NVARCHAR(50),
-  (2, 'des_plate_z', 'xxx', 'ignored',    'yyy', NULL,       'xyz',       'abcde', '12345'), -- going to be ignored because we get the latest run
-  (3, 'des_plate_1', 'A01', 'plate_123',  'A01', NULL,       'sample_001','rna_1', 'lab_1'),
-  (3, 'des_plate_1', 'A02', 'plate_123',  'A02', NULL,       'sample_002','rna_2', 'lab_1'),
-  (3, 'des_plate_1', 'A03', 'plate_456',  'A01', NULL,       'sample_003','rna_3', 'lab_2'),
-  (3, 'des_plate_1', 'A04', 'plate_789',  'A01', NULL,       'sample_004','rna_4', 'lab_2'),
-  (3, 'des_plate_1', 'A05', 'c_plate_1',  'C03', 'positive', NULL,        NULL,    NULL),
-  (3, 'des_plate_1', 'A06', 'plate_abc',  'A01', NULL,       'sample_005','rna_5', 'lab_3');
+  (2, 'des_plate_z', 'xxx', 'ignored',    'yyy', NULL,       'xyz',       'abcde', '12345', 'plate_1'), -- going to be ignored because we get the latest run
+  (3, 'des_plate_1', 'A01', 'plate_123',  'A01', NULL,       'sample_001','rna_1', 'lab_1', 'plate_2'),
+  (3, 'des_plate_1', 'A02', 'plate_123',  'A02', NULL,       'sample_002','rna_2', 'lab_1', 'plate_3'),
+  (3, 'des_plate_1', 'A03', 'plate_456',  'A01', NULL,       'sample_003','rna_3', 'lab_2', 'plate_4'),
+  (3, 'des_plate_1', 'A04', 'plate_789',  'A01', NULL,       'sample_004','rna_4', 'lab_2', 'plate_5'),
+  (3, 'des_plate_1', 'A05', 'c_plate_1',  'C03', 'positive', NULL,        NULL,    NULL, 'plate_6'),
+  (3, 'des_plate_1', 'A06', 'plate_abc',  'A01', NULL,       'sample_005','rna_5', 'lab_3', 'plate_7');
--- tests/fixtures/data/dart.py
+++ tests/fixtures/data/dart.py
@@ -9,6 +9,7 @@ from lighthouse.constants.fields import (
     FIELD_DART_ROOT_SAMPLE_ID,
     FIELD_DART_SOURCE_BARCODE,
     FIELD_DART_SOURCE_COORDINATE,
+    FIELD_DART_LH_SAMPLE_UUID,
     FIELD_LAB_ID,
     FIELD_LH_SAMPLE_UUID,
     FIELD_PLATE_BARCODE,
@@ -16,6 +17,7 @@ from lighthouse.constants.fields import (
     FIELD_RNA_ID,
     FIELD_ROOT_SAMPLE_ID,
     FIELD_SOURCE,
+    FIELD_LH_SAMPLE_UUID
 )
 
 DART_MONGO_MERGED_SAMPLES = [
@@ -30,6 +32,7 @@ DART_MONGO_MERGED_SAMPLES = [
             FIELD_DART_ROOT_SAMPLE_ID: "",
             FIELD_DART_RNA_ID: "",
             FIELD_DART_LAB_ID: "",
+            FIELD_DART_LH_SAMPLE_UUID: "plate_3"
         },
     },
     {  # Non-control sample
@@ -43,6 +46,7 @@ DART_MONGO_MERGED_SAMPLES = [
             FIELD_ROOT_SAMPLE_ID: "MCM002",
             FIELD_RNA_ID: "rna_2",
             FIELD_LAB_ID: "AP",
+            FIELD_LH_SAMPLE_UUID: "plate_3"
         },
         "row": {
             FIELD_DART_DESTINATION_COORDINATE: "B02",
@@ -53,6 +57,7 @@ DART_MONGO_MERGED_SAMPLES = [
             FIELD_DART_ROOT_SAMPLE_ID: "MCM002",
             FIELD_DART_RNA_ID: "rna_2",
             FIELD_DART_LAB_ID: "AB",
+            FIELD_LH_SAMPLE_UUID: "plate_3"
         },
     },
 ]
