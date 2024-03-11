95510368df0b25a4869b8c1694cc4f9badda4e41
yoldas <ay6@sanger.ac.uk>
2024-03-11 19:30:23 +0000

    Previous change proved that the test was incorrect

--- tests/helpers/test_dart.py
+++ tests/helpers/test_dart.py
@@ -23,8 +23,6 @@ def test_find_dart_source_samples_rows_not_found_barcode(app, dart_samples):
 
 def test_exceptions_are_propagated_up(app, dart_samples):
     with app.app_context():
-        with patch("lighthouse.helpers.dart.create_dart_connection", side_effect=Exception()):
-            found = find_dart_source_samples_rows("unknown") # XXX: Raise exception here
-            with raises(Exception):
-                found = find_dart_source_samples_rows("unknown")
-                assert found is None
+        with patch("lighthouse.helpers.dart.create_dart_connection", side_effect=Exception("test exception")):
+            with raises(Exception, match="test exception"):
+                find_dart_source_samples_rows("unknown")
