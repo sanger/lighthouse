74f98ccd59d802b29fba093e1a544d3ffe1d2eb1
Dasun Pubudumal <pubudumald@gmail.com>
2025-01-24 11:25:15 +0000

    Update Python version to 3.12 and change SQL Server ODBC driver to version 18

--- setup_sqlserver_test_db.py
+++ setup_sqlserver_test_db.py
@@ -6,7 +6,7 @@ LOCALHOST = os.environ.get("LOCALHOST", "127.0.0.1")
 
 cnxn = pyodbc.connect(
     (
-        "DRIVER={ODBC Driver 17 for SQL Server};"  # noqa: F541
+        "DRIVER={ODBC Driver 18 for SQL Server};"  # noqa: F541
         f"SERVER=tcp:{LOCALHOST};"
         "DATABASE=master;UID=SA;PWD=MyS3cr3tPassw0rd"
     ),
