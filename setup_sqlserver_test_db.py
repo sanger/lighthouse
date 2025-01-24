8e9396935200526cdb2581c7ec1941e8d71853ec
Dasun Pubudumal <pubudumald@gmail.com>
2025-01-24 11:38:47 +0000

    Add TrustServerCertificate option to SQL Server connection string

--- setup_sqlserver_test_db.py
+++ setup_sqlserver_test_db.py
@@ -8,7 +8,7 @@ cnxn = pyodbc.connect(
     (
         "DRIVER={ODBC Driver 18 for SQL Server};"  # noqa: F541
         f"SERVER=tcp:{LOCALHOST};"
-        "DATABASE=master;UID=SA;PWD=MyS3cr3tPassw0rd"
+        "DATABASE=master;UID=SA;PWD=MyS3cr3tPassw0rd;TrustServerCertificate=yes"
     ),
     autocommit=True,
 )
